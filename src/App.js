7ce0f4c99968326827c2f1c3892a97e21da0c161
Patrick <muilipato@gmail.com>
2024-07-31 00:12:57 +0300

    added image in place of logo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
+import image from './NIC_0674.JPG';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={image} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
