b0a002f7356e4ef59ffe39a1739cafdcab1cd555
Patrick <muilipato@gmail.com>
2024-07-31 00:56:01 +0300

    added hero section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 //import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Header from './components/Header';
+import Hero from './components/Hero';
 import Footer from './components/Footer';
 
 function App() {
   return (
     <>
     <Header />
+    <Hero/>
     <Footer/>
     </>
 
---
+++ src/components/Hero.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Hero.css';
+
+const Hero = () => (
+
+    <div className="hero-text">
+      <h1>Welcome to Our Service</h1>
+      <p>Join us and feel the presence of God.</p>
+    </div>
+
+);
+
+export default Hero;
