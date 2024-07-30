39355a02879d1729c96c3d168629ae282e0db9f0
Patrick <muilipato@gmail.com>
2024-07-31 00:47:08 +0300

    added footer component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 //import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
-      <Header />
+    <>
+    <Header />
+    <Footer/>
+    </>
+
   );
 }
 
