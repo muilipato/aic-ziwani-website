f91e37674f7754e1a6e6f07a69ae43fba56b6f6e
Patrick Maweu <muilipato@gmail.com>
2024-08-09 19:12:31 +0300

    added about page

---
+++ src/pages/About.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Hero from '../components/Hero';
+
+const About = () => (
+  <div className="about-page">
+    <Hero/>
+    <section className="history">
+      <h2>Brief Church History</h2>
+      <p>Our church was founded in ...</p>
+    </section>
+    <section className="pastoral-team">
+      <h2>Pastoral Team</h2>
+      <div className="team-cards">
+        <div className="team-card">
+          <img src="path-to-image.jpg" alt="Pastor Name" />
+          <p>Pastor Name</p>
+        </div>
+        {/* Add more team cards as needed */}
+      </div>
+    </section>
+  </div>
+);
+
+export default About;
