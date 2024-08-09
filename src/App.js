66a6841e70faf361eb933bc15d9767b48c51739e
Patrick Maweu <muilipato@gmail.com>
2024-08-09 19:13:20 +0300

    added route for about page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router , Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import About from './pages/About';
 function App() {
   return (
     <Router basename="/aic-ziwani-website">
       <Routes>
         <Route exact path = '/' element={<Home/>}></Route>
+        <Route path ='/about' element = {<About/>}></Route>
       </Routes>
     </Router>
   
