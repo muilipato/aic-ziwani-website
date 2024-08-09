5ae84e39ff3eed608e0555d045df0f755ce3b32b
Patrick Maweu <muilipato@gmail.com>
2024-08-09 20:05:17 +0300

    added backgroundimage prop

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Hero.css';
 
 const Hero = ({title, subtitle, backgroundImage}) => (
-  <section className="hero" style={{ backgroundImage: 'url(${backgroundImage})' }}>
+  <section className="hero" style={{ backgroundImage: `url(${backgroundImage})` }}>
     <div className="hero-text">
       <h1>{title}</h1>
       <p>{subtitle}</p>
