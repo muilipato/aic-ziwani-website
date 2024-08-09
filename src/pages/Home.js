3f9cf1f025856aa449dcb883d041d3a9af0d682a
Patrick Maweu <muilipato@gmail.com>
2024-08-09 20:06:24 +0300

    added background image on home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,11 +4,12 @@ import Hero from '../components/Hero';
 import Services from '../components/Services';
 import Sermons from '../components/Sermons';
 import Footer from '../components/Footer';
+import homeBackgroundHeroImage from '../images/308585658_411706434441516_7217764519872752177_n.jpg'
 
 const Home = () => (
   <div>
     <Header />
-    <Hero title="Welcome to Our Service" subtitle="Join us and feel the presence of God."/>
+    <Hero title="Welcome to Our Service" subtitle="Join us and feel the presence of God." backgroundImage={homeBackgroundHeroImage}/>
     <Services />
     <Sermons />
     <Footer />
