ebfdcd84fb063e80f02623487e8a371066a2c060
Patrick Maweu <muilipato@gmail.com>
2024-08-09 19:28:35 +0300

    added more nav links

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,6 +9,9 @@ const Header = () => (
       <ul>
         <li><Link to='/'>Home</Link></li>
         <li><Link to='/about'>About</Link></li>
+        <li><Link to="/youth">Youth</Link></li>
+        <li><Link to="/events">Events</Link></li>
+        <li><Link to="/leadership">Leadership</Link></li>
       </ul>
     </nav>
   </header>
