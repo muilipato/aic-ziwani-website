84b673c14bc6466a0917b6483e7957c5e55bb053
Patrick Maweu <muilipato@gmail.com>
2024-08-10 12:22:58 +0300

    added logo to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Header.css';
+import logo from '../images/aic_logo.png'; // Import the logo image
 
 const Header = () => (
   <header className="header">
-    <div className="logo">Church Name</div>
-    <nav>
-      <ul>
-        <li><Link to='/'>Home</Link></li>
-        <li><Link to='/about'>About</Link></li>
-        <li><Link to="/youth">Youth</Link></li>
-        <li><Link to="/events">Events</Link></li>
-        <li><Link to="/leadership">Leadership</Link></li>
-      </ul>
+    <div className="logo-container">
+      <img src={logo} alt="AIC Ziwani Logo" className="logo" />
+      <h1 className="church-name">AIC Ziwani</h1>
+    </div>
+    <nav className="nav-menu">
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+      <Link to="/youth">Youth</Link>
+      <Link to="/events">Events</Link>
+      <Link to="/leadership">Leadership</Link>
     </nav>
   </header>
 );
